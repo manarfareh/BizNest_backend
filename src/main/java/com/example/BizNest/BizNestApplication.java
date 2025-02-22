b629c43f6bc89e39e1966ab7b49fb6b7e1dcdd28
manarfareh <farehmanar89@gmail.com>
2025-02-22 21:58:08 +0100

    first commit

---
+++ src/main/java/com/example/BizNest/BizNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.BizNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BizNest/BizNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BizNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
